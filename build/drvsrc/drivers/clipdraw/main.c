308f2fdb92d174d336ba8d2e26f668f3d2a440c1
Alex Boxall <alexboxall3@gmail.com>
2023-01-14 08:26:45 +1100

    4

--- build/drvsrc/drivers/clipdraw/main.c
+++ build/drvsrc/drivers/clipdraw/main.c
@@ -11,21 +11,25 @@
 #include "region/region.h"
 #include "region/draw.h"
 
+
+void demo_window_draw(int x, int y, int w, int h, bool has_focus) {
+    int shadow_width = has_focus ? 12 : 8;
+
+    for (int i = 0; i < shadow_width; ++i) {
+        struct region shadow = region_create_rounded_rectangle(x + i, y + i, w - 2 * i, h - 2 * i, 10 + shadow_width - i);
+        region_fill(&shadow, (1 << (i / 8)) << 24);
+    }
+
+    struct region rgn = region_create_rounded_rectangle(x + shadow_width, y + shadow_width, w - shadow_width * 2, h - shadow_width * 2, 10);
+
+    region_fill(&rgn, 0xFFF2F0F0);
+}
+
 void _driver_entry_point() {
-    struct region bg = region_create_rectangle(0, 0, 800, 600);
+    struct region bg = region_create_rectangle(0, 0, 1200, 800);
     region_fill(&bg, 0xFF60D0F0);
 
-    struct region shadow_1 = region_create_rounded_rectangle(100, 100, 500, 400, 10);
-    struct region shadow_2 = region_create_rounded_rectangle(101, 101, 499, 399, 10);
-    struct region shadow_3 = region_create_rounded_rectangle(102, 102, 498, 398, 10);
-    struct region shadow_4 = region_create_rounded_rectangle(103, 103, 497, 397, 10);
-    struct region shadow_5 = region_create_rounded_rectangle(104, 104, 496, 396, 10);
-    struct region rgn = region_create_rounded_rectangle(105, 105, 490, 390, 10);
-
-    region_fill(&shadow_1, 0x04000000);
-    region_fill(&shadow_2, 0x08000000);
-    region_fill(&shadow_3, 0x10000000);
-    region_fill(&shadow_4, 0x20000000);
-    region_fill(&shadow_5, 0x40000000);
-    region_fill(&rgn, 0xFFF2F0F0);
+    
+    demo_window_draw(100, 100, 500, 400, false);
+    demo_window_draw(300, 250, 400, 350, true);
 }
--- drivers/x86/clipdraw/main.c
+++ drivers/x86/clipdraw/main.c
@@ -11,21 +11,32 @@
 #include "region/region.h"
 #include "region/draw.h"
 
+
+void demo_window_draw(int x, int y, int w, int h, bool has_focus) {
+    int shadow_width = has_focus ? 12 : 8;
+
+    struct region previous = region_create_rectangle(x, y, 0, 0, 0);
+
+    for (int i = 0; i < shadow_width; ++i) {
+        struct region current = region_create_rounded_rectangle(x + i, y + i, w - 2 * i, h - 2 * i, 10 + shadow_width - i);
+        struct region clipped = region_operate(previous, current, REGION_OP_DIFFERENCE);
+        region_fill(&current, (1 << (i / 8)) << 24);
+
+        region_destroy(previous);
+        region_destroy(clipped);
+        previous = current;
+    }
+
+    struct region rgn = region_create_rounded_rectangle(x + shadow_width, y + shadow_width, w - shadow_width * 2, h - shadow_width * 2, 10);
+
+    region_fill(&rgn, 0xFFF2F0F0);
+}
+
 void _driver_entry_point() {
-    struct region bg = region_create_rectangle(0, 0, 800, 600);
+    struct region bg = region_create_rectangle(0, 0, 1200, 800);
     region_fill(&bg, 0xFF60D0F0);
 
-    struct region shadow_1 = region_create_rounded_rectangle(100, 100, 500, 400, 10);
-    struct region shadow_2 = region_create_rounded_rectangle(101, 101, 498, 398, 10);
-    struct region shadow_3 = region_create_rounded_rectangle(102, 102, 496, 396, 10);
-    struct region shadow_4 = region_create_rounded_rectangle(103, 103, 494, 394, 10);
-    struct region shadow_5 = region_create_rounded_rectangle(104, 104, 492, 392, 10);
-    struct region rgn = region_create_rounded_rectangle(105, 105, 490, 390, 10);
-
-    region_fill(&shadow_1, 0x01000000);
-    region_fill(&shadow_2, 0x04000000);
-    region_fill(&shadow_3, 0x10000000);
-    region_fill(&shadow_4, 0x10000000);
-    region_fill(&shadow_5, 0x10000000);
-    region_fill(&rgn, 0xFFF2F0F0);
+    
+    demo_window_draw(100, 100, 500, 400, false);
+    demo_window_draw(300, 250, 400, 350, true);
 }
